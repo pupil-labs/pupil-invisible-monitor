0ba64857f3dbae2138d28b6e3ba609717731d09e
Pablo Prietz <pablo@prietz.org>
2019-11-04 15:34:05 +0100

    Remove offset filter

--- src/pupil_invisible_monitor/__main__.py
+++ src/pupil_invisible_monitor/__main__.py
@@ -1,11 +1,10 @@
 import logging.handlers
 from pathlib import Path
 
-from .filter import OffsetFilter
 from .models import Host_Controller
 from .overlay import GazeOverlay
 from .texture import PITextureController
-from .ui import HostViewController, OffsetFilterViewController
+from .ui import HostViewController
 from .window import Window
 
 
@@ -29,16 +28,13 @@ def main():
     try:
         host_controller = Host_Controller()
 
-        offset_filter = OffsetFilter()
-        host_controller.add_observer("on_recent_gaze", offset_filter.filter_gaze)
-
         # frame observer
         texture_controller = PITextureController()
         host_controller.add_observer("on_host_linked", texture_controller.reset)
         host_controller.add_observer("on_recent_frame", texture_controller.update)
 
         gaze_overlay = GazeOverlay()
-        offset_filter.add_observer("on_filtered_gaze", gaze_overlay.update)
+        host_controller.add_observer("on_recent_gaze", gaze_overlay.update)
 
         win = Window(
             texture_controller,
@@ -50,10 +46,6 @@ def main():
             ],
         )
         win.open()
-        offset_filter_view_controller = OffsetFilterViewController(
-            gui_parent=win.gui, controller=offset_filter
-        )
-        win.add_observer("on_click", offset_filter_view_controller.on_click)
         host_view_controller = HostViewController(
             gui_parent=win.quickbar, controller=host_controller
         )
@@ -70,7 +62,6 @@ def main():
         win.close()
         host_controller.cleanup()
         host_view_controller.cleanup()
-        offset_filter_view_controller.cleanup()
         logging.shutdown()
 
 
