28e4564f06697b56b772c9eda5d76fb1c074cd65
Pablo Prietz <pablo@prietz.org>
2019-11-11 17:50:27 +0100

    Improve consistency of gaze visualization between Monitor and Companion

--- src/pupil_invisible_monitor/overlay.py
+++ src/pupil_invisible_monitor/overlay.py
@@ -2,7 +2,7 @@ from pyglui.cygl.utils import RGBA, draw_circle, draw_points
 
 
 class GazeOverlay:
-    def __init__(self, ring_size=80):
+    def __init__(self, ring_size=160):
         self.ring_size = ring_size
         self._recent_gaze = None
 
@@ -11,7 +11,7 @@ class GazeOverlay:
             draw_circle(
                 center_position=self._recent_gaze,
                 radius=self.ring_size,
-                stroke_width=20,
+                stroke_width=30,
                 color=RGBA(1.0, 0.0, 0.0, 0.6),
                 sharpness=0.8,
             )
